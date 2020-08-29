5a0e6675924d593365bc4e3e0e237b04ed7ac7bd
Mlk777 <>
2020-08-29 13:48:12 +0200

    moving from heroku to github pages

--- src/App.vue
+++ src/App.vue
@@ -34,26 +34,26 @@
 
 <script>
 export default {
-  name: "app",
+  name: 'app',
   data() {
     return {
-      markdown: ""
+      markdown: '',
     };
   },
   mounted: function() {
     document.body.style.margin = 0;
-    document.body.style.overflow = "hidden";
+    document.body.style.overflow = 'hidden';
   },
   methods: {
     download: function(filename, text) {
-      let element = document.createElement("a");
+      let element = document.createElement('a');
       element.setAttribute(
-        "href",
-        "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+        'href',
+        'data:text/plain;charset=utf-8,' + encodeURIComponent(text)
       );
-      element.setAttribute("download", filename);
+      element.setAttribute('download', filename);
 
-      element.style.display = "none";
+      element.style.display = 'none';
       document.body.appendChild(element);
 
       element.click();
@@ -62,16 +62,16 @@ export default {
     },
     triggerDownload: function() {
       let markdown = this.$refs.markdown;
-      if (markdown.value != "") {
+      if (markdown.value != '') {
         let text = markdown.value;
-        let filename = "newfile.md";
+        let filename = 'newfile.md';
 
         this.download(filename, text);
       } else {
         window.alert("You can't download an empty file");
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
@@ -90,17 +90,7 @@ body {
 .app {
   overflow: hidden;
   height: 100vh;
-  background: #757f9a; /* fallback for old browsers */
-  background: -webkit-linear-gradient(
-    to right,
-    #d7dde8,
-    #757f9a
-  ); /* Chrome 10-25, Safari 5.1-6 */
-  background: linear-gradient(
-    to right,
-    #d7dde8,
-    #757f9a
-  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  background: #c0d2d8;
 }
 .title {
   font-size: 1.5rem;
@@ -157,7 +147,6 @@ textarea {
   background: white;
   border-radius: 5px;
   overflow-y: auto;
-  border-left: 1px solid #0000000d;
 }
 
 #download-button {
